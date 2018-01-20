113b7bc3153dacd83b2a162eb59e64c48b6e7337
Christian Ramsey <thechristianramsey@gmail.com>
2018-01-20 10:23:50 -0800

    added batch size as arg

--- reading_data.py
+++ reading_data.py
@@ -6,7 +6,7 @@ feature_names = ['lat', 'lng', 'ad', "altitude", 'time_before', 'date', 'time', 
 feat_defaults = [ [0.],  [0.],  [0.], [0.],        [0.],         ['na'],   ['na'],  [0]  ]
 
 
-def my_input_fn(file_path, perform_shuffle=False, repeat_count=1):
+def my_input_fn(file_path, batch_size = 32, perform_shuffle=False, repeat_count=1):
    def decode_csv(line):
        parsed_line = tf.decode_csv(line, feat_defaults)
        label = parsed_line[-1:] # Last element is the label
@@ -22,7 +22,7 @@ def my_input_fn(file_path, perform_shuffle=False, repeat_count=1):
        # Randomizes input using a window of 256 elements (read into memory)
        dataset = dataset.shuffle(buffer_size=256)
    dataset = dataset.repeat(repeat_count) # Repeats dataset this # times
-   dataset = dataset.batch(32)  # Batch size to use
+   dataset = dataset.batch(batch_size)  # Batch size to use
    iterator = dataset.make_one_shot_iterator()
    batch_features, batch_labels = iterator.get_next()
    return batch_features, batch_labels
@@ -32,5 +32,14 @@ myds = my_input_fn(file_path)
 
 sess = tf.Session()
 from pprint import pprint
-pprint(sess.run(myds))
+# pprint(sess.run(myds))
 
+
+
+
+next_batch = my_input_fn(file_path, batch_size=1, perform_shuffle=True) # Will return 32 random elements
+
+
+with tf.Session() as sess:
+    first_batch = sess.run(next_batch)
+pprint(first_batch)
