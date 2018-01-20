87e16ae43f6134900ed4e2f42c41a2ce9b4b2edd
Christian Ramsey <thechristianramsey@gmail.com>
2018-01-20 10:09:21 -0800

    added basic run for the tensorflow experiment/estimator input function

---
+++ reading_data.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import numpy as np
+
+file_path = "data/dataset.csv"
+feature_names = ['lat', 'lng', 'ad', "altitude", 'time_before', 'date', 'time', 'y']
+feat_defaults = [ [0.],  [0.],  [0.], [0.],        [0.],         ['na'],   ['na'],  [0]  ]
+
+
+def my_input_fn(file_path, perform_shuffle=False, repeat_count=1):
+   def decode_csv(line):
+       parsed_line = tf.decode_csv(line, feat_defaults)
+       label = parsed_line[-1:] # Last element is the label
+       del parsed_line[-1] # Delete last element
+       features = parsed_line # Everything (but last element) are the features
+       d = dict(zip(feature_names, features)), label
+       return d
+
+   dataset = (tf.data.TextLineDataset(file_path) # Read text file
+       # .skip(1) # Skip header row
+       .map(decode_csv)) # Transform each elem by applying decode_csv fn
+   if perform_shuffle:
+       # Randomizes input using a window of 256 elements (read into memory)
+       dataset = dataset.shuffle(buffer_size=256)
+   dataset = dataset.repeat(repeat_count) # Repeats dataset this # times
+   dataset = dataset.batch(32)  # Batch size to use
+   iterator = dataset.make_one_shot_iterator()
+   batch_features, batch_labels = iterator.get_next()
+   return batch_features, batch_labels
+
+
+myds = my_input_fn(file_path)
+
+sess = tf.Session()
+from pprint import pprint
+pprint(sess.run(myds))
+
